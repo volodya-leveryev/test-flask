27db99a7c8cd076ff5c004df35381afa68d32b56
Vladimir Leveryev <volodya.leveryev@gmail.com>
2022-09-30 17:25:47 +0900

    Add database support

--- mivt22/__init__.py
+++ mivt22/__init__.py
@@ -1,9 +1,16 @@
 from flask import Flask
+from . import database
 from . import views
 
 
 def create_app():
     app = Flask(__name__)
+
+    # TODO: put the database URI in config file
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    database.db.init_app(app)
 
     app.add_url_rule("/", view_func=views.hello_world)
 
